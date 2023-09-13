b4e55a188995964360d7d105b9745e77df1cac1f
khanhtrinh301103 <s3927152@rmit.edu.vn>
2023-09-13 11:43:16 +0700

    finish rentMotorbike search engine

--- MotoBikeRenSED19-kent/MotoBikeRenSED19-kent/UserUI.h
+++ MotoBikeRenSED19-kent/MotoBikeRenSED19-kent/UserUI.h
@@ -146,49 +146,120 @@ public:
     }
 
     static void rentMotorbike() {
-        std::cout << "Enter the city where you want to rent a motorbike: ";
-        std::string searchCity;
-        std::cin.ignore();
-        std::getline(std::cin, searchCity);
+    // Define a struct to represent a motorbike
+    struct Motorbike {
+        int MotorbikeID;
+        std::string Model;
+        std::string Color;
+        int EngineSize;
+        std::string TransmissionMode;
+        int YearMade;
+        std::string Description;
+        int OwnerID;
+        float ExpectedRentedRate;
+        int RateID;
+        std::string ProductStatus;
+        int Score;
+        std::string Comment;
+        std::string City;
+    };
 
-        std::ifstream productDetailFile("ProductDetail.txt");
-        bool foundMotorbikeInCity = false;
-        bool foundProductStatusAvailable = false;
+    // Open the file by name (without specifying a directory)
+    std::ifstream inputFile("ProductDetail.txt");
 
-        std::string line;
-        std::vector<std::string> motorbikeDetails;
+    if (!inputFile.is_open()) {
+        std::cerr << "Error: Unable to open the input file." << std::endl;
+        return;
+    }
 
-        while (std::getline(productDetailFile, line)) {
-            if (line.find("City: " + searchCity) != std::string::npos) {
-                foundMotorbikeInCity = true; // Found the city
+    std::vector<Motorbike> motorbikes;
+    Motorbike currentMotorbike;
+
+    // Read motorbike data and populate the vector
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        if (line.find("MotorbikeID: ") != std::string::npos) {
+            // If a new motorbike entry is found, push the current one to the vector
+            if (currentMotorbike.MotorbikeID != 0) {
+                motorbikes.push_back(currentMotorbike);
             }
-            if (line.find("Product status: available") != std::string::npos) {
-                foundProductStatusAvailable = true; // Found product status available
-
-                // Display motorbike details if both conditions are met
-                if (foundMotorbikeInCity) {
-                    for (const std::string& detail : motorbikeDetails) {
-                        std::cout << detail << std::endl;
-                    }
-                    std::cout << std::endl;
-                }
-
-                motorbikeDetails.clear(); // Clear the details for the next motorbike
-                foundMotorbikeInCity = false; // Reset the city flag
-            }
-
-            motorbikeDetails.push_back(line);
-            if (motorbikeDetails.size() > 13) {
-                motorbikeDetails.erase(motorbikeDetails.begin()); // Limit to 13 lines before the current line
-            }
-        }
-
-        productDetailFile.close();
-
-        if (!foundProductStatusAvailable) {
-            std::cout << "No available motorbikes found in " << searchCity << " City." << std::endl;
+            // Reset the currentMotorbike
+            currentMotorbike = Motorbike();
+            sscanf(line.c_str(), "MotorbikeID: %d", &currentMotorbike.MotorbikeID);
+        } else if (line.find("Model: ") != std::string::npos) {
+            currentMotorbike.Model = line.substr(7);
+        } else if (line.find("Color: ") != std::string::npos) {
+            currentMotorbike.Color = line.substr(7);
+        } else if (line.find("Engine Size: ") != std::string::npos) {
+            sscanf(line.c_str(), "Engine Size: %d", &currentMotorbike.EngineSize);
+        } else if (line.find("Transmission Mode: ") != std::string::npos) {
+            currentMotorbike.TransmissionMode = line.substr(19);
+        } else if (line.find("Year Made: ") != std::string::npos) {
+            sscanf(line.c_str(), "Year Made: %d", &currentMotorbike.YearMade);
+        } else if (line.find("Description: ") != std::string::npos) {
+            currentMotorbike.Description = line.substr(13);
+        } else if (line.find("Owner ID: ") != std::string::npos) {
+            sscanf(line.c_str(), "Owner ID: %d", &currentMotorbike.OwnerID);
+        } else if (line.find("Expected Rented Rate: ") != std::string::npos) {
+            sscanf(line.c_str(), "Expected Rented Rate: %f%%", &currentMotorbike.ExpectedRentedRate);
+        } else if (line.find("Rate ID: ") != std::string::npos) {
+            sscanf(line.c_str(), "Rate ID: %d", &currentMotorbike.RateID);
+        } else if (line.find("Product status: ") != std::string::npos) {
+            currentMotorbike.ProductStatus = line.substr(16);
+        } else if (line.find("Score: ") != std::string::npos) {
+            sscanf(line.c_str(), "Score: %d", &currentMotorbike.Score);
+        } else if (line.find("Comment: ") != std::string::npos) {
+            currentMotorbike.Comment = line.substr(9);
+        } else if (line.find("City: ") != std::string::npos) {
+            currentMotorbike.City = line.substr(6);
         }
     }
+
+    // Push the last motorbike entry to the vector
+    if (currentMotorbike.MotorbikeID != 0) {
+        motorbikes.push_back(currentMotorbike);
+    }
+
+    // Close the file
+    inputFile.close();
+
+    // Prompt the user for the desired city
+    std::string desiredCity;
+    std::cout << "Enter the city where you want to rent a motorbike: ";
+    std::cin.ignore();
+    std::getline(std::cin, desiredCity);
+
+    // Display available motorbikes in the desired city
+    std::cout << "The motorbikes available for " << desiredCity << " city:" << std::endl;
+
+    bool found = false;
+
+    for (const Motorbike& bike : motorbikes) {
+        if (bike.City == desiredCity && bike.ProductStatus == "available") {
+            // Display the motorbike information
+            std::cout << "MotorbikeID: " << bike.MotorbikeID << std::endl;
+            std::cout << "Model: " << bike.Model << std::endl;
+            std::cout << "Color: " << bike.Color << std::endl;
+            std::cout << "Engine Size: " << bike.EngineSize << std::endl;
+            std::cout << "Transmission Mode: " << bike.TransmissionMode << std::endl;
+            std::cout << "Year Made: " << bike.YearMade << std::endl;
+            std::cout << "Description: " << bike.Description << std::endl;
+            std::cout << "Owner ID: " << bike.OwnerID << std::endl;
+            std::cout << "Expected Rented Rate: " << bike.ExpectedRentedRate << "%" << std::endl;
+            std::cout << "Rate ID: " << bike.RateID << std::endl;
+            std::cout << "Product status: " << bike.ProductStatus << std::endl;
+            std::cout << "Score: " << bike.Score << std::endl;
+            std::cout << "Comment: " << bike.Comment << std::endl;
+            std::cout << "City: " << bike.City << std::endl;
+            std::cout << std::endl;
+            found = true;
+        }
+    }
+
+    if (!found) {
+        std::cout << "No available motorbikes found in " << desiredCity << " city." << std::endl;
+    }
+}
 };
 
 #endif // USERUI_H
